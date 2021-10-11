c8c384500da0b98be2beb255e058e7b037a0858a
stylelinz <cwchuang1997@gmail.com>
2021-10-11 11:03:56 +0800

    feat(user): add create service & controller

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private userRepo: Repository<User>) {}
+
+  create(email: string, password: string) {
+    const user = this.userRepo.create({ email, password });
+
+    return this.userRepo.save(user);
+  }
+}
