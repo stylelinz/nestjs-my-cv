9c998d4bb4ebcde4f2c14adf03a8cb6e8f980b85
stylelinz <cwchuang1997@gmail.com>
2021-12-26 21:59:14 +0800

    test: add auth user with cookie

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -15,16 +15,32 @@ describe('Authentication (e2e)', () => {
     await app.init();
   });
 
-  it('handles a signup request', () => {
+  it('handles a signup request', async () => {
     const email = 'tsetsts@test.com';
-    return request(app.getHttpServer())
+    const res = await request(app.getHttpServer())
       .post('/auth/signup')
       .send({ email, password: 'asldflerq' })
-      .expect(201)
-      .then((res) => {
-        const { id, email } = res.body;
-        expect(id).toBeDefined();
-        expect(email).toEqual(email);
-      });
+      .expect(201);
+    const { id, email: email_1 } = res.body;
+    expect(id).toBeDefined();
+    expect(email_1).toEqual(email);
+  });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'asdf@asdf.com';
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'asdf' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
   });
 });
