e672e097079c90018e27347dadc1e3986435a668
stylelinz_mac <cwchuang1997@gmail.com>
2022-01-29 10:05:03 +0800

    teat: add reports init test case

--- src/reports/reports.controller.spec.ts
+++ src/reports/reports.controller.spec.ts
@@ -1,12 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { User } from '../users/user.entity';
+import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsController } from './reports.controller';
+import { Report } from './reports.entity';
+import { ReportsService } from './reports.service';
 
 describe('ReportsController', () => {
   let controller: ReportsController;
+  let mockReportsService: Partial<ReportsService>;
 
   beforeEach(async () => {
+    mockReportsService = {
+      create: (createReportDto: CreateReportDto, user: User) => {
+        return Promise.resolve({
+          ...createReportDto,
+          user,
+        } as Report);
+      },
+    };
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ReportsController],
+      providers: [
+        {
+          provide: ReportsService,
+          useValue: mockReportsService,
+        },
+      ],
     }).compile();
 
     controller = module.get<ReportsController>(ReportsController);
