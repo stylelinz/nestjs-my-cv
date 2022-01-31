bc1c55f9e56549b5ed57693edaee13a8d7aa0304
stylelinz_mac <cwchuang1997@gmail.com>
2022-01-31 14:31:46 +0800

    chore: add swagger api doc

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+
+const APP_PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
+
+  const config = new DocumentBuilder()
+    .addCookieAuth()
+    .setTitle('My CV')
+    .addServer(`http://localhost:${APP_PORT}`)
+    .build();
+
+  const apiDocument = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('doc', app, apiDocument);
+
+  await app.listen(APP_PORT).catch(console.error);
+
+  console.log(`🚀 Service start at http://localhost:${APP_PORT}`);
 }
 bootstrap();
